2036ec5376685d243ffd01f36b6491aaaced4f34
Asus <Asus@BEHZAD>
2023-01-28 17:42:11 +0330

    Add project files.

---
+++ IDisposable/Program.cs
@@ -0,0 +1,23 @@
+﻿// class must implement IDisposable inorder to be accessible in using()
+using (var instance = new MyClass())
+{
+    Console.WriteLine(instance.MyFunction());
+}
+
+
+
+
+class MyClass : IDisposable
+{
+    public int MyProperty = new Random().Next(0, 10);
+    public int MyFunction()
+    {
+        return MyProperty;
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine("Dispose...");
+        GC.SuppressFinalize(this);
+    }
+}
